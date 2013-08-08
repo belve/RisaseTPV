9a93de6b61bceb0bab94af0953cae3705f5f11a5
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-08 12:51:16 +0200

    ee

--- variables.php
+++ variables.php
@@ -11,7 +11,7 @@ $equiEST['T']="ENVIADO A TIENDAS";
 global $dbnivel; global $tiendas; global $dbnivelCR; global $dbnivelAPP;
 
 $dbnivelAPP=new DB('192.168.1.11','tpv','tpv','risase');
-$dbnivel=new DB('localhost','root','2010dos','RisaseTPV');
+$dbnivel=new DB('localhost','root','tpv','RisaseTPV');
 
 
 
