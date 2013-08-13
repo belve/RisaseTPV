73921bf386495c8ac731497d2b7e07687b6c203b
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-13 12:10:14 +0200

    ss

---
+++ functions/print.php
@@ -0,0 +1,21 @@
+
+
+<?php
+
+
+
+$ticket="
+RISASE SA
+collares	X1	22,95
+Pulseras	X3	22.96
+ 
+Total			33.25 €
+";
+
+$ticket=iconv('UTF-8', 'ASCII//TRANSLIT', $ticket). "\n\n\n\n\n\n\n\n\n";
+
+$fp = fopen("lpt1", "r+");
+fwrite($fp,$ticket);
+
+
+?>
