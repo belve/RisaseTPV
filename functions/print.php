3a83af75bed508b456a0bb4cb9a06eec4d9bb9de
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-13 14:03:05 +0200

    ddd

--- functions/print.php
+++ functions/print.php
@@ -12,7 +12,7 @@ Pulseras	X3	22.96
 Total			33.25 €
 ";
 
-$ticket=iconv('UTF-8', 'ASCII//TRANSLIT', $ticket). "\n\n\n\n\n\n\n\n\n";
+$ticket=iconv('UTF-8', 'ASCII//TRANSLIT', $ticket). "\n\n\n\n\n\n\n\n\n" . chr(27) . chr(112) . chr(48)  . chr(100) ;
 
 $fp = fopen("LPT1:", "r+");
 fwrite($fp,$ticket);
@@ -20,5 +20,5 @@ fwrite($fp,$ticket);
 
 #$data = "test.txt"; 
 #echo exec("type ".$data." > lpt1");  
-#echo $ticket;
+#echo $ticket; Chr(27) + Chr(112) + Chr(48) + Chr(100)
 ?>
