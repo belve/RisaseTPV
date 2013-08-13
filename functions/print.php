430bb9cd6db759321501454aafc5e17c1a7fd844
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-08-13 13:38:03 +0200

    ddd

--- functions/print.php
+++ functions/print.php
@@ -14,11 +14,11 @@ Total			33.25 €
 
 $ticket=iconv('UTF-8', 'ASCII//TRANSLIT', $ticket). "\n\n\n\n\n\n\n\n\n";
 
-$fp = fopen("LPT1:", "r+");
-fwrite($fp,$ticket);
+#$fp = fopen("LPT1:", "r+");
+#fwrite($fp,$ticket);
 
 
 $data = "test.txt"; 
-exec("type ".$data." > lpt1");  
-echo $ticket;
+echo exec("type ".$data." > lpt1");  
+#echo $ticket;
 ?>
