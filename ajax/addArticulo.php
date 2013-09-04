9eca833ca95d90edc5cb26db77beabd919882d88
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-04 12:18:18 +0200

    ggg

--- ajax/addArticulo.php
+++ ajax/addArticulo.php
@@ -3,7 +3,7 @@ foreach($_GET as $nombre_campo => $valor){  $asignacion = "\$" . $nombre_campo .
 require_once("../db.php");
 require_once("../variables.php");
 
-
+$manual=str_replace('-','',$manual);
 
 if (!$dbnivel->open()){die($dbnivel->error());};
 
