6c4ccfa7be8eee4dc8b92e83bf70d8a78fccbe2e
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-16 16:05:38 +0200

    ddd

--- functions/print.php
+++ functions/print.php
@@ -100,6 +100,87 @@ Send_print($ticket,$ticket2,$ticket3);
 }
 
 
+
+
+
+
+
+function caja($det){
+
+
+$espacios[0] ="";
+$espacios[1] =" ";
+$espacios[2] ="  ";
+$espacios[3] ="   ";
+$espacios[4] ="    ";
+$espacios[5] ="     ";
+$espacios[6] ="      ";
+$espacios[7] ="       ";
+$espacios[8] ="        ";
+$espacios[9] ="         ";
+$espacios[10]="          ";
+$espacios[11]="           ";
+$espacios[12]="            ";
+$espacios[13]="             ";
+$espacios[14]="              ";
+$espacios[15]="               ";
+$espacios[16]="                ";
+$espacios[17]="                 ";
+$espacios[18]="                  ";
+$espacios[19]="                   ";
+$espacios[20]="                    ";
+$espacios[21]="                     ";
+$espacios[22]="                      ";
+$espacios[23]="                       ";
+$espacios[24]="                        ";
+$espacios[25]="                         ";
+$espacios[26]="                          ";
+$espacios[27]="                           ";
+$espacios[28]="                            ";
+$espacios[29]="                             ";
+$espacios[30]="                              ";
+$espacios[31]="                               ";
+$espacios[32]="                                ";
+
+
+
+$fecha=date('d') . "/" . date('m') . "/" . date('Y');
+
+
+
+ 
+$ticket ="Caja                      Fecha:$fecha\n\n";
+#$ticket.="Articulo       Codigo  Cant  Precio  Total\n";
+#$ticket.="Empleado                           vendido\n";
+$tot=0;
+foreach ($det as $nom => $sum) {$tot=$tot+$sum;
+$s=$espacios[42-strlen($nom)-strlen($sum)]; $sum=$s . $sum;
+$ticket.= $nom . $sum ."\n";		
+}
+
+
+$ticket.="__________________________________________\n";
+
+$tot=number_format($tot,2);$s=$espacios[36-strlen($tot)]; $tot=$s . $tot;
+$ticket.="Total:$tot\n";
+
+$ticket.="\n\n\n\n\n";	
+	
+	
+$ticket2 = chr(29) . chr(86) . chr(48)  . chr(0) ;
+$ticket3 = chr(27) . chr(112) . chr(0)  . chr(25) . chr(250);
+
+
+Send_print($ticket,$ticket2,$ticket3);	
+}
+
+
+
+
+
+
+
+
 function Send_print($ticket,$ticket2,$ticket3){
 
 $ticket=iconv('UTF-8', 'ASCII//TRANSLIT', $ticket);
