624cca03d2a401ca0cef7408f35f4ca8488625f9
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-09-18 14:08:26 +0200

    ss

--- sync/sync_ticket.php
+++ sync/sync_ticket.php
@@ -1,5 +1,9 @@
 <?php
 
+$debug=1;
+
+if($debug){echo "ticket ________________________- \n\n";};
+
 $art="";$restos=array();$tickets=array();$pedir=array();$noconectado=0;$tickdone=array();$articulos=array();$tosync=array();
 
 if (!$dbnivel->open()){die($dbnivel->error());};
@@ -14,8 +18,10 @@ $tickets[$row['id_ticket']]['idt']=$row['id_tienda'];
 $tickets[$row['id_ticket']]['des']=$row['descuento'];	
 };
 
+if($debug){echo "$queryp \n\n";};
+
 $queryp= "select id_ticket, id_articulo, cantidad, importe, (select id from articulos where codbarras=id_articulo) as idart FROM ticket_det;";
-$dbnivel->query($queryp);$count=0;
+$dbnivel->query($queryp);$count=0;if($debug){echo "$queryp \n\n";};
 while ($row = $dbnivel->fetchassoc()){$count++;
 $tickets[$row['id_ticket']]['det'][$count][$row['id_articulo']]['qty']=$row['cantidad'];
 $tickets[$row['id_ticket']]['det'][$count][$row['id_articulo']]['imp']=$row['importe'];
@@ -29,7 +35,7 @@ $restos[$row['id_articulo']]=$restos[$row['id_articulo']] + $row['cantidad'];
 $art=substr($art,0,strlen($art)-1);
 
 $queryp= "select cod, stock, alarma, (select congelado from articulos where codbarras=cod) as congelado from stocklocal where cod IN ($art);";
-$dbnivel->query($queryp);
+$dbnivel->query($queryp);if($debug){echo "$queryp \n\n";};
 while ($row = $dbnivel->fetchassoc()){
 $articulos[$row['cod']]['s']=$row['stock'];	
 $articulos[$row['cod']]['a']=$row['alarma'];
@@ -59,10 +65,10 @@ foreach ($tickets as $cticket => $valores){
 $id_tienda=$valores['idt']; $id_empleado=$valores['emp']; $fecha=$valores['dat']; $importe=$valores['imp']; $desc=$valores['des'];
 
 $queryp= "insert into tickets (id_tienda, id_ticket, id_empleado, fecha, importe, descuento) values ('$id_tienda', '$cticket', '$id_empleado', '$fecha', '$importe', '$desc');";
-$dbnivelAPP->query($queryp);
+$dbnivelAPP->query($queryp);if($debug){echo "$queryp \n\n";};
 
 $queryp="select id_ticket from tickets where id IN(SELECT LAST_INSERT_ID() from tickets);";
-$dbnivelAPP->query($queryp);
+$dbnivelAPP->query($queryp);if($debug){echo "$queryp \n\n";};
 while ($row = $dbnivelAPP->fetchassoc()){$tickdone[$row['id_ticket']]=1;};
 }
 
@@ -74,7 +80,7 @@ $catidad=$datos['qty'];
 $importe=$datos['imp'];
 		
 $queryp= "insert into ticket_det (id_tienda, id_ticket, id_articulo, cantidad, importe) values ('$id_tienda', '$idhecho', '$codidbar', '$catidad', '$importe');";
-$dbnivelAPP->query($queryp);
+$dbnivelAPP->query($queryp);if($debug){echo "$queryp \n\n";};
 		
 	
 }}
@@ -101,25 +107,25 @@ if(!array_key_exists($codbar, $articulos)){$articulos[$codbar]['s']=0;$articulos
 	
 if(($articulos[$codbar]['s'] - $resto <= $articulos[$codbar]['a'])&&($articulos[$codbar]['c']<1)){
 $queryp= "insert into pedidos (codbarras) values ('$codbar');";
-$dbnivel->query($queryp);		
+$dbnivel->query($queryp);	if($debug){echo "$queryp \n\n";};	
 } #genera pedido
 
 
 
 $idstl="";
 $queryp= "select id from stocklocal where cod=$codbar ";
-$dbnivel->query($queryp);
+$dbnivel->query($queryp);if($debug){echo "$queryp \n\n";};
 while ($row = $dbnivel->fetchassoc()){$idstl=$row['id'];};
 
 $idart2=$idartis2[$codbar];
 		
 if($idstl){			
 $queryp= "update stocklocal set stock=stock - $resto where cod=$codbar;";
-$dbnivel->query($queryp);	$tosync[]=$queryp;
+$dbnivel->query($queryp);	$tosync[]=$queryp;if($debug){echo "$queryp \n\n";};
 }else{
 $resto=$resto * (-1);
 $queryp= "insert into stocklocal (id_art,cod,stock,alarma) values ('$idart2','$codbar','$resto','0');";
-$dbnivel->query($queryp);	$tosync[]=$queryp;
+$dbnivel->query($queryp);	$tosync[]=$queryp;if($debug){echo "$queryp \n\n";};
 }
 
 
@@ -131,10 +137,10 @@ $dbnivel->query($queryp);	$tosync[]=$queryp;
 foreach ($tickdone as $idhecho => $point){
 	
 $queryp= "delete from tickets where id_ticket='$idhecho';";
-$dbnivel->query($queryp);	
+$dbnivel->query($queryp);	if($debug){echo "$queryp \n\n";};
 
 $queryp= "delete from ticket_det where id_ticket='$idhecho';";
-$dbnivel->query($queryp);	
+$dbnivel->query($queryp);	if($debug){echo "$queryp \n\n";};
 
 echo "Procesado Ticket: $idhecho \n";
 	
