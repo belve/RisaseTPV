1e907b65a4b2d8c058f2803922ada9b52aab56e0
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2013-11-24 21:39:55 +0100

    ww

--- ajax/regalo.php
+++ ajax/regalo.php
@@ -14,7 +14,7 @@ while ($row = $dbnivel->fetchassoc()){$nt=$row['ciudad']; $dr=$row['direccion'];
 $queryp= "select id_articulo, 
 cantidad, 
 (select nombre from subgrupos where id_grupo = (substring(id_articulo,1,1)) AND clave=(substring(id_articulo,2,1)) ) as ngru
- from dev_ticket_det where id_ticket='$t';"; 
+ from dev_ticket_det where id_ticket='$t' AND cantidad > 0;"; 
 $dbnivel->query($queryp);$point=0;
 while ($row = $dbnivel->fetchassoc()){$point++;
 
