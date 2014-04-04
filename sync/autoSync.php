86868632d0a222d6a4d3163abad75c3cc2f1bf23
Eduardo Buenadicha Moya <ebmoya@Casa.(none)>
2014-04-04 12:17:07 +0200

    yy

--- sync/autoSync.php
+++ sync/autoSync.php
@@ -20,7 +20,7 @@ $fecha = new DateTime($hoy);
 $fecha->sub(new DateInterval('P90D'));
 $bttDEV= $fecha->format('Y-m-d');
 
-include('sync_cuadra.php');       echo "__________________________________________________ 1 \n ";
+
 include('sync_general.php');      echo "__________________________________________________ 2 \n ";
 include('sync_getpedidos.php');
 include('sync_ticket.php');  
@@ -29,6 +29,7 @@ include('sync_roturas.php');
 include('sync_fijStock.php');  echo "__________________________________________________ 3 \n ";
 include('sync_fijPVP.php');
 
+include('sync_cuadra.php');       echo "__________________________________________________ 1 \n ";
 
 echo "------ to sync FINAL ----- \n";
 print_r($tosync);
